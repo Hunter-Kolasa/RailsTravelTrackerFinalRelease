bb1e4a34a2d994736065eb895cbbaabde4706b47
EliasAHH <eliasiscoding@gmail.com>
2021-01-28 10:40:04 -0800

    Omniauth lesson/session completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to movies_path 
         else 
-            redirect_to "/login"
+            redirect_to new_user_path
         end 
     end 
 
